db7da3af3b357760b7f59446a6c66203e33805d1
Gertjan Debusschere <gertjandebusschere@outlook.com>
2025-03-21 11:53:51 +0100

    OpenSuse and corrections added

--- ffs.sh
+++ ffs.sh
@@ -23,13 +23,13 @@ show_menu() {
     echo "7) Install Flatpak and Spotify"
     echo "8) Set default target to graphical and reboot"
     echo "9) Install MS Edge"
-    echo "10) Exit"
-    echo "11) Install Steam"
-    echo "12) Install Google Chrome"
-    echo "13) Install NVtop"
-    echo "14) Install btop"
-    echo "15) Install Wine"
-    echo "16) Install Nvidia drivers and CUDA"
+    echo "10) Install Steam"
+    echo "11) Install Google Chrome"
+    echo "12) Install NVtop"
+    echo "13) Install btop"
+    echo "14) Install Wine"
+    echo "15) Install Nvidia drivers and CUDA"
+    echo "16) Install Desktop Environment"
     echo "17) Exit"
 }
 
@@ -43,6 +43,8 @@ install_components() {
                 sudo apt update && sudo apt upgrade -y
             elif [ "$DISTRO" = "arch" ]; then
                 sudo pacman -Syu
+            elif [ "$DISTRO" = "opensuse" ]; then
+                sudo zypper update -y
             fi
             ;;
         2)
@@ -52,6 +54,8 @@ install_components() {
                 sudo add-apt-repository universe -y
             elif [ "$DISTRO" = "arch" ]; then
                 echo "No equivalent package for Arch Linux"
+            elif [ "$DISTRO" = "opensuse" ]; then
+                echo "No equivalent package for OpenSUSE"
             fi
             ;;
         3)
@@ -62,6 +66,9 @@ install_components() {
             elif [ "$DISTRO" = "arch" ]; then
                 sudo pacman -Syu
                 sudo pacman -S --needed base-devel
+            elif [ "$DISTRO" = "opensuse" ]; then
+                sudo zypper addrepo -f https://download.opensuse.org/repositories/multimedia:/apps/openSUSE_Tumbleweed/ multimedia
+                sudo zypper refresh
             fi
             ;;
         4)
@@ -73,6 +80,8 @@ install_components() {
                 sudo apt install ubuntu-restricted-extras -y
             elif [ "$DISTRO" = "arch" ]; then
                 echo "No equivalent package for Arch Linux"
+            elif [ "$DISTRO" = "opensuse" ]; then
+                sudo zypper install patterns-openSUSE-enhanced_base patterns-openSUSE-multimedia patterns-openSUSE-multimedia_opt
             fi
             ;;
         5)
@@ -85,6 +94,8 @@ install_components() {
                 sudo apt install terminator firefox celluloid libreoffice btop gnome-shell gnome-tweaks gnome-extensions-app gnome-shell-extension-appindicator gnome-disk-utility gnome-software nemo wine steam variety blender solaar -y
             elif [ "$DISTRO" = "arch" ]; then
                 sudo pacman -S terminator firefox celluloid libreoffice-fresh btop gnome-shell gnome-tweaks gnome-extensions-app gnome-shell-extension-appindicator gnome-disk-utility gnome-software nemo wine steam variety blender solaar --noconfirm
+            elif [ "$DISTRO" = "opensuse" ]; then
+                sudo zypper install terminator firefox celluloid libreoffice btop gnome-shell gnome-tweaks gnome-extensions-app gnome-shell-extension-appindicator gnome-disk-utility gnome-software nemo wine steam variety blender solaar -y
             fi
             ;;
         6)
@@ -103,6 +114,12 @@ install_components() {
                 sudo apt install code teams libcxx kdenlive gimp -y
             elif [ "$DISTRO" = "arch" ]; then
                 sudo pacman -S code teams libcxx kdenlive gimp --noconfirm
+            elif [ "$DISTRO" = "opensuse" ]; then
+                sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+                sudo zypper addrepo https://packages.microsoft.com/yumrepos/vscode vscode
+                sudo zypper addrepo https://packages.microsoft.com/yumrepos/ms-teams teams
+                sudo zypper refresh
+                sudo zypper install code teams libcxx kdenlive gimp -y
             fi
             ;;
         7)
@@ -120,6 +137,11 @@ install_components() {
                 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
                 flatpak update
                 flatpak install spotify
+            elif [ "$DISTRO" = "opensuse" ]; then
+                sudo zypper install flatpak -y
+                flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+                flatpak update
+                flatpak install spotify
             fi
             ;;
         8)
@@ -130,6 +152,9 @@ install_components() {
                 sudo systemctl set-default graphical.target
                 sudo reboot
             elif [ "$DISTRO" = "arch" ]; then
+                sudo systemctl set-default graphical.target
+                sudo reboot
+            elif [ "$DISTRO" = "opensuse" ]; then
                 sudo systemctl set-default graphical.target
                 sudo reboot
             fi
@@ -148,21 +173,25 @@ install_components() {
                 sudo apt install microsoft-edge-stable -y
             elif [ "$DISTRO" = "arch" ]; then
                 yay -S microsoft-edge-stable-bin --noconfirm
+            elif [ "$DISTRO" = "opensuse" ]; then
+                sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+                sudo zypper addrepo https://packages.microsoft.com/yumrepos/edge edge
+                sudo zypper refresh
+                sudo zypper install microsoft-edge-stable -y
             fi
             ;;
         10)
-            exit 0
-            ;;
-        11)
             if [ "$DISTRO" = "fedora" ]; then
                 sudo dnf install steam -y
             elif [ "$DISTRO" = "ubuntu" ]; then
                 sudo apt install steam -y
             elif [ "$DISTRO" = "arch" ]; then
                 sudo pacman -S steam --noconfirm
+            elif [ "$DISTRO" = "opensuse" ]; then
+                sudo zypper install steam -y
             fi
             ;;
-        12)
+        11)
             if [ "$DISTRO" = "fedora" ]; then
                 sudo dnf install fedora-workstation-repositories -y
                 sudo dnf config-manager --set-enabled google-chrome
@@ -172,36 +201,46 @@ install_components() {
                 sudo apt install ./google-chrome-stable_current_amd64.deb -y
             elif [ "$DISTRO" = "arch" ]; then
                 yay -S google-chrome --noconfirm
+            elif [ "$DISTRO" = "opensuse" ]; then
+                sudo zypper addrepo https://dl.google.com/linux/direct/google-chrome.repo
+                sudo zypper refresh
+                sudo zypper install google-chrome-stable -y
             fi
             ;;
-        13)
+        12)
             if [ "$DISTRO" = "fedora" ]; then
                 sudo dnf install nvtop -y
             elif [ "$DISTRO" = "ubuntu" ]; then
                 sudo apt install nvtop -y
             elif [ "$DISTRO" = "arch" ]; then
                 sudo pacman -S nvtop --noconfirm
+            elif [ "$DISTRO" = "opensuse" ]; then
+                sudo zypper install nvtop -y
             fi
             ;;
-        14)
+        13)
             if [ "$DISTRO" = "fedora" ]; then
                 sudo dnf install btop -y
             elif [ "$DISTRO" = "ubuntu" ]; then
                 sudo apt install btop -y
             elif [ "$DISTRO" = "arch" ]; then
                 sudo pacman -S btop --noconfirm
+            elif [ "$DISTRO" = "opensuse" ]; then
+                sudo zypper install btop -y
             fi
             ;;
-        15)
+        14)
             if [ "$DISTRO" = "fedora" ]; then
                 sudo dnf install wine -y
             elif [ "$DISTRO" = "ubuntu" ]; then
                 sudo apt install wine -y
             elif [ "$DISTRO" = "arch" ]; then
                 sudo pacman -S wine --noconfirm
+            elif [ "$DISTRO" = "opensuse" ]; then
+                sudo zypper install wine -y
             fi
             ;;
-        16)
+        15)
             if [ "$DISTRO" = "fedora" ]; then
                 sudo dnf install akmod-nvidia -y
                 sudo dnf install xorg-x11-drv-nvidia-cuda -y
@@ -210,7 +249,78 @@ install_components() {
                 sudo apt install nvidia-cuda-toolkit -y
             elif [ "$DISTRO" = "arch" ]; then
                 sudo pacman -S nvidia nvidia-utils cuda --noconfirm
+            elif [ "$DISTRO" = "opensuse" ]; then
+                sudo zypper install nvidia-computeG05 nvidia-gfxG05-kmp-default nvidia-glG05 nvidia-settings nvidia-xconfig cuda -y
             fi
+            ;;
+        16)
+            echo "Choose a desktop environment:"
+            echo "1) Gnome"
+            echo "2) KDE"
+            echo "3) Cinnamon"
+            echo "4) COSMIC"
+            echo "5) XFCE"
+            read -p "Enter your choice [1-5]: " de_choice
+            case $de_choice in
+                1)
+                    if [ "$DISTRO" = "fedora" ]; then
+                        sudo dnf groupinstall "GNOME Desktop Environment" -y
+                    elif [ "$DISTRO" = "ubuntu" ]; then
+                        sudo apt install ubuntu-gnome-desktop -y
+                    elif [ "$DISTRO" = "arch" ]; then
+                        sudo pacman -S gnome gnome-extra --noconfirm
+                    elif [ "$DISTRO" = "opensuse" ]; then
+                        sudo zypper install -t pattern gnome gnome_basic
+                    fi
+                    ;;
+                2)
+                    if [ "$DISTRO" = "fedora" ]; then
+                        sudo dnf groupinstall "KDE Plasma Workspaces" -y
+                    elif [ "$DISTRO" = "ubuntu" ]; then
+                        sudo apt install kubuntu-desktop -y
+                    elif [ "$DISTRO" = "arch" ]; then
+                        sudo pacman -S plasma kde-applications --noconfirm
+                    elif [ "$DISTRO" = "opensuse" ]; then
+                        sudo zypper install -t pattern kde kde_plasma
+                    fi
+                    ;;
+                3)
+                    if [ "$DISTRO" = "fedora" ]; then
+                        sudo dnf groupinstall "Cinnamon Desktop" -y
+                    elif [ "$DISTRO" = "ubuntu" ]; then
+                        sudo apt install cinnamon-desktop-environment -y
+                    elif [ "$DISTRO" = "arch" ]; then
+                        sudo pacman -S cinnamon --noconfirm
+                    elif [ "$DISTRO" = "opensuse" ]; then
+                        sudo zypper install cinnamon
+                    fi
+                    ;;
+                4)
+                    if [ "$DISTRO" = "fedora" ]; then
+                        echo "COSMIC is not available for Fedora"
+                    elif [ "$DISTRO" = "ubuntu" ]; then
+                        echo "COSMIC is not available for Ubuntu"
+                    elif [ "$DISTRO" = "arch" ]; then
+                        sudo pacman -S cosmic --noconfirm
+                    elif [ "$DISTRO" = "opensuse" ]; then
+                        echo "COSMIC is not available for OpenSUSE"
+                    fi
+                    ;;
+                5)
+                    if [ "$DISTRO" = "fedora" ]; then
+                        sudo dnf groupinstall "Xfce Desktop" -y
+                    elif [ "$DISTRO" = "ubuntu" ]; then
+                        sudo apt install xubuntu-desktop -y
+                    elif [ "$DISTRO" = "arch" ]; then
+                        sudo pacman -S xfce4 xfce4-goodies --noconfirm
+                    elif [ "$DISTRO" = "opensuse" ]; then
+                        sudo zypper install -t pattern xfce xfce_basis
+                    fi
+                    ;;
+                *)
+                    echo "Invalid option"
+                    ;;
+            esac
             ;;
         17)
             exit 0
